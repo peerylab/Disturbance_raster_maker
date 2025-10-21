04ef87ffaf77dc3c5713388742e2f39a70d3d482
hakramer <77811641+hakramer@users.noreply.github.com>
2025-10-21 15:30:22 -0500

    Update 3_prep_fires.py

--- 3_prep_fires.py
+++ 3_prep_fires.py
@@ -1,11 +1,13 @@
-# Anu Kramer
-# 11-13-2024
+# Anu Kramer - hakramer@wisc.edu
+# Updated 6-9-2025
 
 # PURPOSE: prep CBI data
 #               o	Project COVA fires, maintaining alignment with MMI
 #               o	Reclass NODATA -> 150
 #               o	Multiply by study_area_rast.tif (fire_CBI_yyyy.tif)
 #               o	Reclass 0-2 -> 0; 3 -> 1; 150 -> 0 (fire_hisev_yyyy_final.tif)
+#               o	Reclass 0-1 -> 0; 2 -> 1; 3 -> 0; 150 -> 0 (fire_modsev_yyyy_final.tif)
+#               o	Reclass 0-1 -> 1; 2-3 -> 0; 150 -> 0 (fire_lowsev_yyyy_final.tif)
 #               o	Reclass 1-3 -> 1; 150 -> 1 (fire_allsev_yyyy_final.tif)
 #               o	Build multi-year all severity fire history, using fire_longevity (fire_allsev_yyyytoyyyy_final.tif)
 # SPEED: 30 min (75 min single year + 20 min multi-year)
@@ -18,22 +20,21 @@ from arcpy import env
 from arcpy.sa import *
 import sys
 import os
-import master
+import master_variables
 
 #################################################
 ############ ADJUST THE VALUES BELOW ############
 #################################################
-base_folder = master.base_folder_master
-coordinate_system = master.coordinate_system_master
-geographic_transform = master.geographic_transform_master
-fires_path = master.fires_path_master
-fire_perimeters_path = master.fire_perimeters_path_master
-snap_tile = master.snap_path_master
-fire_longevity = master.fire_longevity_master
+base_folder = master_variables.base_folder_master
+coordinate_system = master_variables.coordinate_system_master
+geographic_transform = master_variables.geographic_transform_master
+fires_path = master_variables.fires_path_master
+fire_perimeters_path = master_variables.fire_perimeters_path_master
+snap_tile = master_variables.snap_path_master
+fire_longevity = master_variables.fire_longevity_master
 
-#extract_by_mask_coordinates = master.extract_by_mask_coordinates_master
-step2_folder = master.step2_master
-step3_folder = master.step3_master
+step2_folder = master_variables.step2_master
+step3_folder = master_variables.step3_master
 ###############################################################
 ############ EVERYTHING BELOW SHOULD BE GOOD TO GO ############
 ###############################################################
@@ -74,22 +75,10 @@ for year in range(1985-fire_longevity,1985):
         arcpy.conversion.PolygonToRaster(
             in_features=select_fires,
             value_field="ones",
-            #out_rasterdataset=saveTo+"oldfire_allsev_"+str_year+"_toRast.tif",
             out_rasterdataset=saveTo+"oldfire_allsev_"+str_year+"_final.tif",
             cell_assignment="CELL_CENTER",
             priority_field="NONE",
             build_rat="BUILD")
-    # # need to run this to get cells aligned with habitat raster
-    # with arcpy.EnvManager(cellSizeProjectionMethod="PRESERVE_RESOLUTION", snapRaster=snap_tile, cellSize=snap_tile):
-    #     out_raster = arcpy.sa.ExtractByMask(
-    #         in_raster=saveTo+"oldfire_allsev_"+str_year+"_toRast.tif",
-    #         in_mask_data=snap_tile,
-    #         extraction_area="INSIDE",
-    #         analysis_extent=extract_by_mask_coordinates+' GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
-    #     )
-    #     out_raster.save(saveTo+"oldfire_allsev_"+str_year+"_final.tif")
-    
-    #print("clipping to study area...")
     MMI_study_area = Times(saveTo+"oldfire_allsev_"+str_year+"_final.tif",study_area)
     MMI_study_area.save(saveTo+"fire_allsev_"+str_year+"_final.tif")    
 
@@ -109,7 +98,6 @@ for year in range(1985,2024):
     with arcpy.EnvManager(snapRaster=MMI_raster_path):
         arcpy.management.ProjectRaster(
             in_raster=fires_path+"/cbi_cat_"+str_year+".tif",
-            #out_raster=saveTo+"CBI_"+str_year+"_WGS_intermediate.tif",
             out_raster=saveTo+"CBI_"+str_year+"_WGS.tif",
             out_coor_system=coordinate_system,
             resampling_type="NEAREST",
@@ -118,16 +106,6 @@ for year in range(1985,2024):
             Registration_Point=None,
             in_coor_system='PROJCS["NAD_1983_California_Teale_Albers",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Albers"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",-4000000.0],PARAMETER["Central_Meridian",-120.0],PARAMETER["Standard_Parallel_1",34.0],PARAMETER["Standard_Parallel_2",40.5],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]',
             vertical="NO_VERTICAL")
-    # # need to run this to get cells aligned with habitat raster
-    # with arcpy.EnvManager(cellSizeProjectionMethod="PRESERVE_RESOLUTION", snapRaster=snap_tile, cellSize=snap_tile):
-    #     out_raster = arcpy.sa.ExtractByMask(
-    #         in_raster=saveTo+"CBI_"+str_year+"_WGS_intermediate.tif",
-    #         in_mask_data=snap_tile,
-    #         extraction_area="INSIDE",
-    #         analysis_extent=extract_by_mask_coordinates+' GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
-    #     )
-    #     out_raster.save(saveTo+"CBI_"+str_year+"_WGS.tif")
-    # reclassify NODATA -> 150
     out_raster = arcpy.sa.Reclassify(
         in_raster=saveTo+"CBI_"+str_year+"_WGS.tif",
         reclass_field="Value", remap="NODATA 150",
@@ -146,6 +124,20 @@ for year in range(1985,2024):
         missing_values="DATA")
     out_raster.save(saveTo+"fire_hisev_"+str_year+"_final.tif")
     
+    # reclassify 0-1 -> 0; 2 -> 1; 3 -> 0; 150 -> 0 
+    out_raster = arcpy.sa.Reclassify(
+        in_raster=saveTo+"cbi_cat_"+str_year+"_study_area_NODATA150.tif",
+        reclass_field="Value", remap="0 1 0;2 2 1;3 3 0;150 150 0",
+        missing_values="DATA")
+    out_raster.save(saveTo+"fire_modsev_"+str_year+"_final.tif")
+    
+    # reclassify 0-1 -> 1; 2-3 -> 0; 150 -> 0 
+    out_raster = arcpy.sa.Reclassify(
+        in_raster=saveTo+"cbi_cat_"+str_year+"_study_area_NODATA150.tif",
+        reclass_field="Value", remap="0 1 1;2 3 0;150 150 0",
+        missing_values="DATA")
+    out_raster.save(saveTo+"fire_lowsev_"+str_year+"_final.tif")
+    
     # reclassify 0-3 -> 1; 150 -> 0 
     out_raster = arcpy.sa.Reclassify(
         in_raster=saveTo+"cbi_cat_"+str_year+"_study_area_NODATA150.tif",
@@ -153,12 +145,11 @@ for year in range(1985,2024):
         missing_values="DATA")
     out_raster.save(saveTo+"fire_allsev_"+str_year+"_final.tif")
 
-
 print("mosaicing multi-year fire histories")
 saveTo=base_folder+"ANALYSIS/"+step3_folder+"/multi_year_fire/"
 os.makedirs(saveTo, exist_ok=True)
+print("allsev")
 MMI_raster_path = base_folder+"ANALYSIS/"+step2_folder+"/2010/"+snap_tile+"_2010_WGS.tif"
-#for year in range(2014,2018):
 for year in range(1985,2024):
     print(year)
     
@@ -173,22 +164,21 @@ for year in range(1985,2024):
             output_location=saveTo,
             raster_dataset_name_with_extension="fire_allsev_"+str(year-fire_longevity)+"to"+str(year)+"_final.tif",
             coordinate_system_for_the_raster=coordinate_system, pixel_type="8_BIT_UNSIGNED", cellsize=None, number_of_bands=1, mosaic_method="MAXIMUM", mosaic_colormap_mode="FIRST")
-        
-print("mosaicing multi-year high sev fire histories") # NEW
-for year in range(1985+fire_longevity,2024):
-    print(year)
-    
-    for working_year in range(year-fire_longevity,year+1):
-        if working_year == (year-fire_longevity):
-            input_HiSev_rasters_string = base_folder+"ANALYSIS/"+step3_folder+"/"+str(working_year)+"/fire_hisev_"+str(working_year)+"_final.tif" # NEW
-        else:
-            input_HiSev_rasters_string = input_HiSev_rasters_string + ";" + base_folder+"ANALYSIS/"+step3_folder+"/"+str(working_year)+"/fire_hisev_"+str(working_year)+"_final.tif" # NEW
-    with arcpy.EnvManager(snapRaster=MMI_raster_path):
-        arcpy.management.MosaicToNewRaster(
-            input_rasters=input_HiSev_rasters_string,
-            output_location=saveTo,
-            raster_dataset_name_with_extension="fire_hisev_"+str(year-fire_longevity)+"to"+str(year)+"_final.tif",
-            coordinate_system_for_the_raster=coordinate_system, pixel_type="8_BIT_UNSIGNED", cellsize=None, number_of_bands=1, mosaic_method="MAXIMUM", mosaic_colormap_mode="FIRST")
 
+for sev_type in ("hisev", "modsev", "lowsev"):
+    print (sev_type)
+    for year in range(1985+fire_longevity,2024):
+        print(year)
+        for working_year in range(year-fire_longevity,year+1):
+            if working_year == (year-fire_longevity):
+                input_HiSev_rasters_string = base_folder+"ANALYSIS/"+step3_folder+"/"+str(working_year)+"/fire_"+sev_type+"_"+str(working_year)+"_final.tif" # NEW
+            else:
+                input_HiSev_rasters_string = input_HiSev_rasters_string + ";" + base_folder+"ANALYSIS/"+step3_folder+"/"+str(working_year)+"/fire_"+sev_type+"_"+str(working_year)+"_final.tif" # NEW
+        with arcpy.EnvManager(snapRaster=MMI_raster_path):
+            arcpy.management.MosaicToNewRaster(
+                input_rasters=input_HiSev_rasters_string,
+                output_location=saveTo,
+                raster_dataset_name_with_extension="fire_"+sev_type+"_"+str(year-fire_longevity)+"to"+str(year)+"_final.tif",
+                coordinate_system_for_the_raster=coordinate_system, pixel_type="8_BIT_UNSIGNED", cellsize=None, number_of_bands=1, mosaic_method="MAXIMUM", mosaic_colormap_mode="FIRST")
 
 print("DONE!")
